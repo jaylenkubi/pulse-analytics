77db2c0170abcf2fc7cd0ec8b4f7fe1bf65df5ab
jaylenkubi <jaylenkubi@gmail.com>
2025-01-10 19:51:53 -0500

    event dto changes

--- src/shared/dto/events/create-event.dto.ts
+++ src/shared/dto/events/create-event.dto.ts
@@ -1,15 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsISO8601, IsNotEmpty, IsObject, IsOptional, IsString } from "class-validator";
 
 export class CreateEventDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   eventType: string;
 
   @IsObject()
-  @IsNotEmpty()
+  @ApiProperty({required: false})
   payload: Record<string, any>;
 
   @IsISO8601()
-  @IsOptional()
+  @IsOptional() 
+  @ApiProperty()
   timestamp?: string;
 }
