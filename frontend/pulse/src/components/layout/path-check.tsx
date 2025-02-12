2661baf11f1438d9f77adb12cd0f49f645b06acc
jaylenkubi <jaylenkubi@gmail.com>
2025-02-11 21:29:11 -0500

    path and breadcrumbs

--- frontend/pulse/src/components/layout/path-check.tsx
+++ frontend/pulse/src/components/layout/path-check.tsx
@@ -1,24 +1,40 @@
 'use client'
 
 import { usePathname } from "next/navigation"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "../sidebar/app-sidebar"
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "../ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
 
 export function PathCheck({ children, defaultOpen }: { children: React.ReactNode, defaultOpen: boolean }) {
-  const pathname = usePathname()
-  const isLoginPage = pathname === '/login'
+	const pathname = usePathname()
+	const isLoginPage = pathname === '/login'
 
-  if (isLoginPage) {
-    return children;
-  }
+	if (isLoginPage) {
+		return children;
+	}
 
-  return (
-    <SidebarProvider defaultOpen={defaultOpen}>
-      <AppSidebar />
-      <main className="flex-1">
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
-  )
+	return (
+		<SidebarProvider defaultOpen={defaultOpen}>
+			<AppSidebar />
+			<main className="flex-1">
+				<SidebarInset>
+					<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator orientation="vertical" className="mr-2 h-4" />
+						<Breadcrumb>
+							<BreadcrumbList>
+								<BreadcrumbItem>
+									<BreadcrumbLink href={pathname} className="text-foreground">
+										{pathname.split('/').pop()!.charAt(0).toUpperCase() + pathname.split('/').pop()!.slice(1)}
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+							</BreadcrumbList>
+						</Breadcrumb>
+					</header>
+				</SidebarInset>
+				{children}
+			</main>
+		</SidebarProvider>
+	)
 }
